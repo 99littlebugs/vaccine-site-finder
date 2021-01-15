c48d1cf25d360e69555887752e3f768507c35af9
Sharon Kanner <sharonk416@gmail.com>
2021-01-14 20:02:37 -0500

    moved to using puppeteer (actual browser) because ny.gov seemed to put controls in to require javascript

--- scraper.js
+++ scraper.js
@@ -1,14 +1,12 @@
 #!/usr/bin/env node
 
-const axios = require('axios');
-const cheerio = require('cheerio');
 const fs = require("fs");
 const path = require('path');
-
+const puppeteer = require('puppeteer');
 
 const nyGovSites = [{
     name: "NYS DOH Hospital / FQHC Scheduling COVID-19 Sutphin Health Center",
-    link: "https://apps.health.ny.gov/doh2/applinks/cdmspr/2/counties?OpID=50501251",
+    link: "https://apps3.health.ny.gov/doh2/applinks/cdmspr/2/counties?OpID=50501251",
     address: {
         name: "Sutphin Health Center",
         street: "105-04 Sutphin Boulevard",
@@ -19,7 +17,7 @@ const nyGovSites = [{
 },
 {
     name: "Northwell CoVID-19 Vaccination Program: Nassau County - Lake Success",
-    link: "https://apps.health.ny.gov/doh2/applinks/cdmspr/2/counties?OpID=50500075",
+    link: "https://apps3.health.ny.gov/doh2/applinks/cdmspr/2/counties?OpID=50500075",
     address: {
         name: "Lake Success/New Hyde Park",
         street: "1111 Marcus Avenue",
@@ -30,7 +28,7 @@ const nyGovSites = [{
 },
 {
     name: "NYS DOH Hospital/FQHC Appointment Scheduling COVID-19 at Nassau University Medical Center",
-    link: "https://apps.health.ny.gov/doh2/applinks/cdmspr/2/counties?OpID=50500121",
+    link: "https://apps3.health.ny.gov/doh2/applinks/cdmspr/2/counties?OpID=50500121",
     address: {
         name: "Nassau University Medical Center",
         street: "2201 Hempstead Turnpike",
@@ -41,7 +39,7 @@ const nyGovSites = [{
 },
 {
     name: "Northwell CoVID-19 Vaccination Program: Nassau County - Elmont",
-    link: "https://apps.health.ny.gov/doh2/applinks/cdmspr/2/counties?OpID=50501190",
+    link: "https://apps3.health.ny.gov/doh2/applinks/cdmspr/2/counties?OpID=50501190",
     address: {
         name: "NYRA/Belmont",
         street: "2150 Hempstead Turnpike",
@@ -52,7 +50,7 @@ const nyGovSites = [{
 },
 {
     name: "FIRST DOSE - NYS DOH Hospital/FQHC Appointment Scheduling COVID-19 at Mount Sinai South Nassau",
-    link: "https://apps.health.ny.gov/doh2/applinks/cdmspr/2/counties?OpID=50500074",
+    link: "https://apps3.health.ny.gov/doh2/applinks/cdmspr/2/counties?OpID=50500074",
     address: {
         name: "Mount Sinai South Nassau",
         street: "One Healthy Way",
@@ -63,7 +61,7 @@ const nyGovSites = [{
 },
 {
     name: "NYS COVID Vaccine POD - JAVax - Manhattan, N.Y.",
-    link: "https://apps.health.ny.gov/doh2/applinks/cdmspr/2/counties?OpID=50502320",
+    link: "https://apps3.health.ny.gov/doh2/applinks/cdmspr/2/counties?OpID=50502320",
     address: {
         name: "JAVax Manhattan, N.Y.",
         street: "429 11th Avenue",
@@ -74,7 +72,7 @@ const nyGovSites = [{
 },
 {
     name: "Northwell CoVID-19 Vaccination Program: Manhattan - Lenox Hill Hospital",
-    link: "https://apps.health.ny.gov/doh2/applinks/cdmspr/2/counties?OpID=50501473",
+    link: "https://apps3.health.ny.gov/doh2/applinks/cdmspr/2/counties?OpID=50501473",
     address: {
         name: "LHH Einhorn Auditorium",
         street: "131 East 76th Street",
@@ -85,7 +83,7 @@ const nyGovSites = [{
 },
 {
     name: "NC Dept. of Health at Nassau Community College CCB",
-    link: "https://apps.health.ny.gov/doh2/applinks/cdmspr/2/counties?OpID=50500881",
+    link: "https://apps3.health.ny.gov/doh2/applinks/cdmspr/2/counties?OpID=50500881",
     address: {
         name: "Nassau Community College",
         street: "1 Education Drive",
@@ -96,7 +94,7 @@ const nyGovSites = [{
 },
 {
     name: "NCDOH at Yes We Can Community Center",
-    link: "https://apps.health.ny.gov/doh2/applinks/cdmspr/2/counties?OpID=50501806",
+    link: "https://apps3.health.ny.gov/doh2/applinks/cdmspr/2/counties?OpID=50501806",
     address: {
         name: "Yes We Can Community Center",
         street: "141 Garden Street",
@@ -107,41 +105,52 @@ const nyGovSites = [{
 },
 {
     name: "NYS COVID Vaccine POD - Jones Beach State Park - Field 3",
-    link: "https://apps.health.ny.gov/doh2/applinks/cdmspr/2/counties?OpID=50502584",
+    link: "https://apps3.health.ny.gov/doh2/applinks/cdmspr/2/counties?OpID=50502584",
     address: {
         name: "Jones Beach State Park",
         street: "1901 Ocean Parkway",
-        city: "Wantagh", 
+        city: "Wantagh",
         state: "NY",
         zip: "11793"
     }
 }];
 
-let promises = [];
-
-for (const site of nyGovSites) {
-    promises.push(axios.get(site.link).then(res => {
+(async () => {
+    // todo: parallelize?
+    const browser = await puppeteer.launch();
+    const [page] = await browser.pages();
+    for (const site of nyGovSites) {
         site.events = [];
-        const $ = cheerio.load(res.data);
-        const eventsWeb = $(".event-type");
-        for (const event of eventsWeb) {
-            const isAvailable = $(event).find("button").text() !== "Event Full";
-            if (isAvailable) {
-                site.events.push({
-                    date: new Date($(event).find("div div:contains('Date:'):last").first().text().substring(6) + " UTC"),
-                    time: $(event).find("div div:contains('Time:'):last").first().text().substring(6),
-                    appointments: $(event).find("div div:contains('Appointments Available:'):last").first().text().substring(24),
-                    linkId: $(event).parent().attr("id")
+        try {
+            await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36 Edg/87.0.664.75');
+            await page.goto(site.link);
+            await page.waitForSelector("#pagetitle, h1", { timeout: 10000 });
+            const isError = await page.$("h1");
+            if (!isError) {
+                site.events = await page.evaluate(() => {
+                    const events = [];
+                    const eventsWeb = $(".event-type");
+                    for (const event of eventsWeb) {
+                        const isAvailable = $(event).find("button").text() !== "Event Full";
+                        if (isAvailable) {
+                            events.push({
+                                date: new Date($(event).find("div div:contains('Date:'):last").first().text().substring(6) + " UTC").toISOString(),
+                                time: $(event).find("div div:contains('Time:'):last").first().text().substring(6),
+                                appointments: $(event).find("div div:contains('Appointments Available:'):last").first().text().substring(24),
+                                linkId: $(event).parent().attr("id")
+                            });
+                        }
+                    }
+                    return events;
                 });
             }
+        } catch (err) {
+            console.error(site.link + ": " + err); // todo: log oout to node
         }
-    }).catch(err => {
-        console.error(err.message + ": " + err.config.url);
-    }));
-}
+    }
+    await browser.close();
 
-Promise.all(promises).then(() => {
     const data = JSON.stringify({ sites: nyGovSites }, null, 2);
     const filename = path.join("data", "ny_state.json");
     fs.writeFileSync(path.resolve(filename), data);
-});
+})();
